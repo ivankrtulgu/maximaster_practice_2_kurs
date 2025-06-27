7ddb661630a795234cd3d957384c07973a780d17
ivankrtulgu <218090641+ivankrtulgu@users.noreply.github.com>
2025-06-27 16:36:40 +0300

    3-js-task-2-dev написана кастомная валидация без карты

--- 3_Javascript/2_Оформление_заказа/js/script.js
+++ 3_Javascript/2_Оформление_заказа/js/script.js
@@ -1,15 +1,46 @@
-telField = document.getElementById("tel-field");
-mainForm = document.getElementById("main-form");
+const telField = document.getElementById("tel-field");
+const mainForm = document.getElementById("main-form");
 
 isNum = () => {
     symb = telField.value[telField.value.length - 1];
     if (!(symb >= '0' && symb <= '9')) {
-        telField.value = telField.value.substr(0, telField.value.length - 1)
+        telField.value = telField.value.substr(0, telField.value.length - 1);
+    }
+}
+
+checkValidity = () => {
+    const fioField = document.getElementById("fio-field"); 
+    const telField = document.getElementById("tel-field"); 
+    const emailField = document.getElementById("email-field"); 
+    if (fioField.value.trim() === '') {
+        return document.getElementById("fail-text-fio");
+    }    
+    if (telField.value.trim() === '') {
+        return document.getElementById("fail-text-tel");
+    }   
+    if (emailField.value.trim() === '') {
+        return document.getElementById("fail-text-email");
     }
 }
 
 formSubmitEvent = () => {
-    console.log(1);
-}
+    const successText = document.getElementById("success-text"); 
+    const failText = document.getElementById("fail-text"); 
+    for (el of document.querySelectorAll(".fail-text")) {
+        el.classList.add('visually-hidden');
+    }
+    for (el of document.querySelectorAll(".success-text")) {
+        el.classList.add('visually-hidden');
+    }
+    if (!this.checkValidity()) {
+        failText.classList.add('visually-hidden');
+        successText.classList.remove('visually-hidden');
+    }
+    else {
+        successText.classList.add('visually-hidden');
+        failText.classList.remove('visually-hidden');
+        this.checkValidity().classList.remove('visually-hidden');
+    }
+}   
 
 telField.addEventListener("input", isNum)

cbe5fda44193fc93ff59f202f55dcd7858f31699
ivankrtulgu <218090641+ivankrtulgu@users.noreply.github.com>
2025-07-03 23:33:16 +0300

    2-verstka сделан блок slider для главной страницы

--- 2_Верстка/js/script.js
+++ 2_Верстка/js/script.js
@@ -1,6 +1,36 @@
+//Поле поиска в header
+
 document.getElementById('headerSearchButton').addEventListener('click', () => {
     const headerSearchElement = document.getElementById('headerSearch');
     if (headerSearchElement.value.trim() === '') {
         document.getElementById('headerSearch').classList.toggle('header__input--visually-hidden');
     } 
+})
+
+//Кнопки слайдера
+
+const sliderEl = document.querySelector('.slider__images');
+
+document.getElementById('sliderButtonPrev').addEventListener('click', () => {
+    const el = document.querySelector('.slider__image--first');
+    if (el.previousElementSibling) {
+        el.previousElementSibling.classList.add('slider__image--first');
+        el.classList.remove('slider__image--first');
+        sliderEl.scrollTo({
+            left: document.querySelector('.slider__image--first').offsetLeft,
+            behavior: 'smooth'
+        });
+    }
+})
+
+document.getElementById('sliderButtonNext').addEventListener('click', () => {
+    const el = document.querySelector('.slider__image--first');
+    if (el.nextElementSibling) {
+        el.nextElementSibling.classList.add('slider__image--first');
+        el.classList.remove('slider__image--first');
+        sliderEl.scrollTo({
+            left: document.querySelector('.slider__image--first').offsetLeft,
+            behavior: 'smooth'
+        });
+    }
 })

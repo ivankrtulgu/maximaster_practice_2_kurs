03ffe4265be9e9ebecfa5e2c5bf6c5cf7d0a7c1a
ivankrtulgu <218090641+ivankrtulgu@users.noreply.github.com>
2025-07-06 00:01:42 +0300

    2-verstka добавлено всплывающее окно для регистрации и входа в аккаунт, исправлен баг при закрытии burger menu, добавлен заголовок слайдера,

--- 2_Верстка/js/script.js
+++ 2_Верстка/js/script.js
@@ -1,10 +1,20 @@
-//Поле поиска в header
+loginWindow = document.querySelector('.login');
+loginButtonNaveNotAccount = document.getElementById('loginButtonNaveNotAccount');
+loginButtonNaveAccount = document.getElementById('loginButtonHaveAccount');
+
+//Кнопки header
 
 document.getElementById('headerSearchButton').addEventListener('click', () => {
     const headerSearchElement = document.getElementById('headerSearch');
     if (headerSearchElement.value.trim() === '') {
         document.getElementById('headerSearch').classList.toggle('header__input--visually-hidden');
     } 
+})
+
+headerButtonLogIn = document.getElementById('headerButtonLogIn');
+
+headerButtonLogIn.addEventListener('click', () => { 
+    loginWindow.classList.toggle('visually-hidden');
 })
 
 //Кнопки слайдера
@@ -41,3 +51,113 @@ document.getElementById('headerBurgerMenuButton').addEventListener('click', () =
     document.querySelector('.nav__list').classList.toggle('nav__list--active');
 })
 
+// Логин
+
+loginFieldEmail.value = localStorage['user_email'];
+
+loginButtonForgotPassword = document.getElementById('loginButtonForgotPassword');
+loginButtonLogIn = document.getElementById('loginButtonLogIn');
+loginContainer = document.querySelector('.login__container');
+loginTitle  = document.querySelector('.login__title');
+loginButtonClose = document.getElementById('loginButtonClose');
+
+loginFieldRepeatPassword = document.getElementById('loginFieldRepeatPassword');
+loginFieldPassword = document.getElementById('loginFieldPassword');
+
+loginMessagePasswordReset = document.querySelector('.login__message-password-reset');
+loginErrorMessage = document.querySelector('.login__error-message');
+
+const login_singup_toggle = () => {
+    for (let el of document.querySelector('.login__row:last-of-type').children) {
+        el.classList.toggle('visually-hidden');
+    }
+    loginButtonNaveNotAccount.classList.toggle('visually-hidden');
+    loginButtonNaveAccount.classList.toggle('visually-hidden');
+    loginButtonForgotPassword.classList.toggle('visually-hidden');
+    loginContainer.classList.toggle('login__container--singup');
+}
+
+loginButtonNaveNotAccount.addEventListener('click', login_singup_toggle)
+loginButtonNaveAccount.addEventListener('click', login_singup_toggle)
+
+loginButtonNaveNotAccount.addEventListener('click', () => {
+    loginButtonLogIn.textContent = 'Sign Up';
+    loginTitle.textContent = 'Sign Up';
+    loginFieldRepeatPassword.setAttribute('required', true);
+    loginMessagePasswordReset.classList.add('visually-hidden')
+    loginMessageError.classList.add('visually-hidden');
+
+})
+
+loginButtonNaveAccount.addEventListener('click', () => {
+    loginButtonLogIn.textContent = 'Log In';
+    loginTitle.textContent = 'Log In';
+    loginFieldRepeatPassword.removeAttribute('required');
+    loginMessagePasswordReset.classList.add('visually-hidden')
+    loginMessageError.classList.add('visually-hidden');
+
+})
+
+loginButtonClose.addEventListener('click', () => {
+    loginWindow.classList.toggle('visually-hidden');
+    loginMessagePasswordReset.classList.add('visually-hidden')
+    loginMessageError.classList.add('visually-hidden');
+})
+
+// loginButtonLogIn.addEventListener('click', () => {
+//     if (!('visually-hidden' in loginFieldRepeatPassword.classList) && (loginFieldRepeatPassword.value !== loginFieldPassword.value)) {
+//         loginMessagePasswordsNotMatch.classList.remove('visually-hidden');
+//         return;
+//     }
+//     localStorage['user_email'] = loginFieldEmail.value;
+// })
+
+const loginFormIsValid = () => {
+    if ((!(loginFieldRepeatPassword.classList.contains('visually-hidden')) && (loginFieldRepeatPassword.value !== loginFieldPassword.value)) ) {
+        loginErrorMessage.classList.remove('visually-hidden');
+        loginErrorMessage.textContent = 'Указанные пароли не совпадают';
+        return false;
+    }   
+    // if (loginFieldEmail.value.trim() === '' || loginFieldRepeatPassword.value.trim() === '' || loginFieldPassword.value.trim() === '') {
+    //     loginErrorMessage.classList.remove('visually-hidden');
+    //     loginErrorMessage.textContent = 'Часть полей не заполнено!';
+    //     return false;
+    // }
+    return true;
+}   
+
+loginWindow.addEventListener('submit', (event) => {
+    if (!loginFormIsValid()) {
+        event.preventDefault();
+        return;
+    }
+    loginErrorMessage.classList.remove('visually-hidden');
+    localStorage['user_email'] = loginFieldEmail.value;
+})
+
+loginButtonForgotPassword.addEventListener('click', () => {
+    loginMessagePasswordReset.classList.remove('visually-hidden')
+})
+
+singUpShortFormButton = document.getElementById('singUpShortFormButton');
+
+// let singUpShortFormButtonEvent = new Event("click");
+
+singUpShortFormButton.addEventListener('click', () => {
+    localStorage['user_email'] = singUpShortFormInputField.value;
+    loginFieldEmail.value = singUpShortFormInputField.value;
+    loginWindow.classList.toggle('visually-hidden');
+})
+
+loginButtonShowPassword = document.getElementById('loginButtonShowPassword');
+loginButtonShowPassword.addEventListener('click', () => {
+    const type = loginFieldPassword.getAttribute('type');
+    if (type === 'password') {
+        loginFieldPassword.setAttribute('type', 'text');
+        loginFieldRepeatPassword.setAttribute('type', 'text');
+    }
+    else {
+        loginFieldPassword.setAttribute('type', 'password');
+        loginFieldRepeatPassword.setAttribute('type', 'password');
+    }
+})
